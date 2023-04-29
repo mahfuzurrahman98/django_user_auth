e85fad15cff123e18f86dfc32a26feaea2487429
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-04-30 00:00:59 +0600

    validation added

---
+++ authapp/signupform.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    username = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', 'Passwords do not match')
--- authapp/views.py
+++ authapp/views.py
@@ -2,6 +2,8 @@ from django.contrib import auth, messages
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
+from .signupform import SignUpForm
+
 # Create your views here.
 
 
@@ -12,48 +14,49 @@ def login(request):
 def register(request):
 
     if request.method == 'POST':
-        # Get form values
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            # Get form values
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            username = request.POST['username']
+            email = request.POST['email']
+            password = request.POST['password']
+            confirm_password = request.POST['confirm_password']
 
-        # print(first_name, last_name, username, email, password, confirm_password)
-        # return
-
-        # Check if passwords match
-        if password == confirm_password:
-            # Check if username exists
-            if User.objects.filter(username=username).exists():
-                messages.error(request, 'That username is taken')
-                return redirect('register')
-            elif User.objects.filter(email=email).exists():
-                messages.error(request, 'That email is being used')
-                return redirect('/authapp/register')
-            else:
-                # Looks good
-                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-                # Login after register
-                # auth.login(request, user)
-                # messages.success(request, 'You are now logged in')
-                # return redirect('index')
-                user.save()
-                messages.success(request, 'You are now registered and can log in')
-                return redirect('/authapp/login')
-
+            # Check if passwords match
+            if password == confirm_password:
+                # Check if username exists
+                if User.objects.filter(username=username).exists():
+                    messages.error(request, 'That username is taken')
+                    return redirect('register')
+                elif User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('/authapp/register')
+                else:
+                    # Looks good
+                    user = User.objects.create_user(
+                        username=username,
+                        password=password,
+                        email=email, first_name=first_name,
+                        last_name=last_name
+                    )
+                    user.save()
+                    messages.success(
+                        request, 'You are now registered and can log in')
+                    return redirect('/authapp/login')
         else:
-            messages.error(request, 'Passwords do not match')
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f'{field}: {error}')
             return redirect('/authapp/register')
-        
+
     elif request.method == 'GET':
-      return render(request, 'authapp/register.html')
-  
+        return render(request, 'authapp/register.html')
+
     else:
-      messages.error(request, 'Invalid request')
+        messages.error(request, 'Invalid request')
 
 
 def home(request):
     return render(request, 'authapp/home.html')
-    
