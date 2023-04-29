2520082ea28c4e75bb273d013ceb86f7a3cfd230
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-04-29 20:32:21 +0600

    message displayed

--- authapp/views.py
+++ authapp/views.py
@@ -20,8 +20,8 @@ def register(request):
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
 
-        print(first_name, last_name, username, email, password, confirm_password)
-        return
+        # print(first_name, last_name, username, email, password, confirm_password)
+        # return
 
         # Check if passwords match
         if password == confirm_password:
@@ -29,24 +29,23 @@ def register(request):
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'That username is taken')
                 return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'That email is being used')
+                return redirect('/authapp/register')
             else:
-                if User.objects.filter(email=email).exists():
-                    messages.error(request, 'That email is being used')
-                    return redirect('register')
-                else:
-                    # Looks good
-                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-                    # Login after register
-                    # auth.login(request, user)
-                    # messages.success(request, 'You are now logged in')
-                    # return redirect('index')
-                    user.save()
-                    messages.success(request, 'You are now registered and can log in')
-                    return redirect('login')
+                # Looks good
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                # Login after register
+                # auth.login(request, user)
+                # messages.success(request, 'You are now logged in')
+                # return redirect('index')
+                user.save()
+                messages.success(request, 'You are now registered and can log in')
+                return redirect('/authapp/login')
 
         else:
             messages.error(request, 'Passwords do not match')
-            return redirect('register')
+            return redirect('/authapp/register')
         
     elif request.method == 'GET':
       return render(request, 'authapp/register.html')
